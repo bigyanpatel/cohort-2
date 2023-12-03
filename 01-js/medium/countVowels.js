3226d620cd90d4159f6be14d52177327e7595399
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-04 03:54:40 +0530

    Solved: count vowels

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,24 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  // return str.split('').reduce((acc, char) => {
+  //   if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+  //     return acc + 1;
+  //   } else {
+  //     return 0;
+  //   }
+  // }, 0)
+
+  let count = 0;
+
+  for(let i = 0; i < str.length; i++){
+    const char = str.toLowerCase().charAt(i);
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 module.exports = countVowels;
