01eaff9f6aa818abf9c03f673939d59b8c0dd07b
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-05 15:34:38 +0530

    Remove: unwanted code countVowels

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,14 +6,6 @@
 */
 
 function countVowels(str) {
-  // return str.split('').reduce((acc, char) => {
-  //   if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
-  //     return acc + 1;
-  //   } else {
-  //     return 0;
-  //   }
-  // }, 0)
-
   let count = 0;
 
   for(let i = 0; i < str.length; i++){
