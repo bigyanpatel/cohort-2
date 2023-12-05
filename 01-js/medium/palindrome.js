a432b74e42c3447a65c6b875af5d8fe74005d1c5
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-05 15:35:23 +0530

    Solved: palindrom problem

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,7 +3,38 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+function isAlphanumeric(char) {
+  const code = char.charCodeAt(0);
+  return (
+    (code >= 48 && code <= 57) ||
+    (code >= 65 && code <= 90) ||
+    (code >= 97 && code <= 122)
+  );
+}
+
 function isPalindrome(str) {
+  // let cleanedString = '';
+  // str = str.toLowerCase(); 
+
+  // for (let i = 0; i < str.length; i++) {
+  //   if (isAlphanumeric(str[i])) {
+  //     cleanedString += str[i];
+  //   }
+  // }
+
+  const cleanedString = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  let left = 0;
+  let right = cleanedString.length - 1;
+
+  while (left < right) {
+    if (cleanedString[left] !== cleanedString[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
   return true;
 }
 
