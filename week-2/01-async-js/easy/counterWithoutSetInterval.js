3e6bd4082441e8c81c9b297d987dcf76f65de59d
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-10 15:40:04 +0530

    Solved: counter without setInterval

---
+++ week-2/01-async-js/easy/counterWithoutSetInterval.js
@@ -0,0 +1,14 @@
+// Function to update and display the counter
+function updateCounter(counter) {
+    // Display the current counter value
+    console.log(`Counter: ${counter}`);
+  
+    // Increment the counter and schedule the next update after 1 second
+    setTimeout(() => {
+      updateCounter(counter + 1); // Call updateCounter recursively after 1 second
+    }, 1000); // 1000 milliseconds = 1 second
+  }
+  
+  // Start the counter from 1
+  updateCounter(1);
+  
