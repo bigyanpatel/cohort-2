d9e384706e2647f7eb66f338908d10b48b6f4a0b
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-12 19:17:07 +0530

    Solved: promise all

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,60 +6,50 @@
  */
 
 // Function that returns a promise resolving after 1 second
-function waitOneSecond() {
-    return new Promise(resolve => {
-      // Set timeout to resolve the promise after 1 second (1000 milliseconds)
-      setTimeout(() => {
-        resolve('Resolved after 1 second');
-      }, 1000);
-    });
-  }
-  
-  // Function that returns a promise resolving after 2 seconds
-  function waitTwoSeconds() {
-    return new Promise(resolve => {
-      // Set timeout to resolve the promise after 2 seconds (2000 milliseconds)
-      setTimeout(() => {
-        resolve('Resolved after 2 seconds');
-      }, 2000);
-    });
-  }
-  
-  // Function that returns a promise resolving after 3 seconds
-  function waitThreeSeconds() {
-    return new Promise(resolve => {
-      // Set timeout to resolve the promise after 3 seconds (3000 milliseconds)
-      setTimeout(() => {
-        resolve('Resolved after 3 seconds');
-      }, 3000);
-    });
-  }
-  
-  // Function that sequentially calls all three functions and measures the time taken
-  function calculateTime() {
-    // Record the start time using Date.now()
-    const startTime = Date.now();
-  
-    // Sequentially call the functions and wait for each promise to resolve in order
-    waitOneSecond()
-      .then(result => {
-        // Log the result and chain the next function call
-        console.log(result);
-        return waitTwoSeconds();
-      })
-      .then(result => {
-        // Log the result and chain the next function call
-        console.log(result);
-        return waitThreeSeconds();
-      })
-      .then(result => {
-        // Log the result and calculate total time taken after all promises resolve
-        console.log(result);
-        const endTime = Date.now();
-        const elapsedTime = endTime - startTime;
-        console.log(`Entire operation took ${elapsedTime} milliseconds`);
-      });
-  }
-  
-  // Call calculateTime function to sequentially execute promises and calculate time
-  calculateTime();  
+function waitOneSecond(t1) {
+  return new Promise(resolve => {
+    // Set timeout to resolve the promise after 1 second (1000 milliseconds)
+    setTimeout(() => {
+      resolve('Resolved after 1 second');
+    }, t1 * 1000);
+  });
+}
+
+// Function that returns a promise resolving after 2 seconds
+function waitTwoSeconds(t) {
+  return new Promise(resolve => {
+    // Set timeout to resolve the promise after 2 seconds (2000 milliseconds)
+    setTimeout(() => {
+      resolve('Resolved after 2 seconds');
+    }, t * 1000);
+  });
+}
+
+// Function that returns a promise resolving after 3 seconds
+function waitThreeSeconds(t) {
+  return new Promise(resolve => {
+    // Set timeout to resolve the promise after 3 seconds (3000 milliseconds)
+    setTimeout(() => {
+      resolve('Resolved after 3 seconds');
+    }, t * 1000);
+  });
+}
+
+// Function that sequentially calls all three functions and measures the time taken
+async function calculateTime(t1, t2, t3) {
+  // Record the start time using Date.now()
+  const startTime = Date.now();
+
+  return waitOneSecond(t1).then(() => waitTwoSeconds(t2)).then(() => waitThreeSeconds(t3)).then(() => {
+    const endTime = Date.now();
+
+    const elapsedTime = endTime - startTime;
+
+    return elapsedTime;
+  });
+}
+
+// Call calculateTime function to sequentially execute promises and calculate time
+calculateTime();
+
+module.exports = calculateTime;
